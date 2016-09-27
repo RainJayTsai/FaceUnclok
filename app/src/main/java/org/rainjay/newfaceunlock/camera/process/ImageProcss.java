17002d5585d73af72525c24049e6ee2290453b85
rainjay <RainJay0117@gmail.com>
2016-09-27 15:30:19 +0800

    create IpUtil.cropFace

--- app/src/main/java/org/rainjay/newfaceunlock/camera/process/ImageProcss.java
+++
@@ -1,9 +0,0 @@
-package org.rainjay.newfaceunlock.camera.process;
-
-/**
- * Created by RainJay on 2016/9/26.
- */
-
-public interface ImageProcss {
-    public byte[] processImage(byte[] data, int width, int height);
-}
---
+++ app/src/main/java/org/rainjay/newfaceunlock/imageutil/IpUtil.java
@@ -0,0 +1,8 @@
+package org.rainjay.newfaceunlock.imageutil;
+
+/**
+ * Created by RainJay on 2016/9/27.
+ */
+
+public class IpUtil {
+}
